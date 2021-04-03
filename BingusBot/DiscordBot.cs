85fc1586123f70da7a21f7c2b8eaf8d11a0f25b5
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2021-04-03 18:47:46 +0100

    Setup base bot structure

---
+++ BingusBot/DiscordBot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace BingusBot
+{
+    public class DiscordBot
+    {
+        private readonly ILogger<DiscordBot> _logger;
+        public DiscordBot(ILogger<DiscordBot> logger)
+        {
+            _logger = logger;
+        }
+        public async Task Run(string[] args) => await InitBot(args);
+
+        async Task InitBot(string[] args)
+        {
+            _logger.LogInformation("Hello world");
+        }
+    }
+}
--- BingusBot/Program.cs
+++ BingusBot/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace BingusBot
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Cancellation token to end bot where required
+        /// </summary>
+        private CancellationTokenSource _cancellationTokenSource { get; set; }
+        
+        /// <summary>
+        /// Contains app config
+        /// </summary>
+        private IConfigurationRoot _config;
+        
+        /// <summary>
+        /// Discord client
+        /// </summary>
+        private DiscordClient _discord;
+        
+        /// <summary>
+        /// Module for discord commands
+        /// </summary>
+        private CommandsNextModule _commands;
+        
+        /// <summary>
+        /// Module for discord interactivity
+        /// </summary>
+        private InteractivityModule _interactivity;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+            {
+                DiscordBot bot = serviceProvider.GetService<DiscordBot>();
+                if (bot != null) await bot.Run(args);
+            }
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<DiscordBot>();
+            services.AddLogging
+            (
+                builder =>
+                {
+                    builder.SetMinimumLevel(LogLevel.Debug);
+                    builder.AddNLog("nlog.config");
+                }
+            );
+            
+            // TODO: Add Caching
         }
     }
 }
