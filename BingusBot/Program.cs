3ebc93f4b84b0394bf5f43e91f72564f46f4b165
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2021-04-03 17:57:59 +0100

    Initial Commit

---
+++ BingusBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BingusBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
