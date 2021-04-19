ba4e5d01c0f923e055a4d0d88033c0e2c8a5378d
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2021-04-19 17:18:25 +0100

    Added TimersCommandModule

---
+++ BingusBot/Commands/TimersCommandModule.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+
+namespace BingusBot.Commands
+{
+    public class TimersCommandModule : IModule
+    {
+        private readonly ILogger<TimersCommandModule> _logger;
+        
+        public TimersCommandModule(ServiceProvider services)
+        {
+            _logger = services.GetService<ILogger<TimersCommandModule>>();
+        }
+
+        [Command("timertest")]
+        public async Task TimerTest(CommandContext context)
+        {
+            await context.Channel.SendMessageAsync("Working");
+        }
+    }
+}
