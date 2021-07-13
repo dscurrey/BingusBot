dbdaf16d722346c24de2dcc84ca4240dcde674dc
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2021-07-13 21:40:26 +0100

    Created new project

---
+++ BingusBot2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace BingusBot2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient
+            (
+                new DiscordConfiguration
+                {
+                    Token = "",
+                    TokenType = TokenType.Bot,
+                    Intents = DiscordIntents.AllUnprivileged
+                }
+            );
+        }
+    }
+}
